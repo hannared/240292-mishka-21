9b4993b9281fcbcd099f80f10cc45ddd432d9c01
hannared <hanna.korsak@gmail.com>
2021-01-24 11:52:05 +0100

    add if

--- source/js/app.js
+++ source/js/app.js
@@ -2,16 +2,20 @@ const icon = document.querySelector('.user-list__icon--cart');
 const text = document.querySelector('.user-list__item--cart');
 
 function cartHover() {
-  icon.classList.toggle('user-list__icon--hover');
-  text.classList.toggle('user-list__icon--hover');
+  if (icon && text) {
+    icon.classList.toggle('user-list__icon--hover');
+    text.classList.toggle('user-list__icon--hover');
+  }
 }
 
 function load() {
-  icon.addEventListener('mouseover', cartHover, false);
-  icon.addEventListener('mouseout', cartHover, false);
+  if (icon && text) {
+    icon.addEventListener('mouseover', cartHover, false);
+    icon.addEventListener('mouseout', cartHover, false);
 
-  text.addEventListener('mouseover', cartHover, false);
-  text.addEventListener('mouseout', cartHover, false);
+    text.addEventListener('mouseover', cartHover, false);
+    text.addEventListener('mouseout', cartHover, false);
+  }
 }
 
 window.addEventListener('load', load, false);
